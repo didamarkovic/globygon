c3f64e78c564b81f663a683da7fa6c4fe127d9b9
Brian Jackson <bjackson@boisestate.edu>
2021-06-22 15:50:03 -0600

    Added first draft of our script

---
+++ globygon.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+class Catalog:
+    def __init__(self, RA, Dec):
+        
+        self.RA = RA
+        self.Dec = Dec
+
+        self.center_of_mass = None
+
+    def _convert_RADec_to_Cartesian(self):
+        '''
+        RA/Dec should be in radians
+        '''
+    
+        x = np.cos(self.Dec)*np.cos(self.RA)
+        y = np.cos(self.Dec)*np.sin(self.RA)
+        z = np.sin(self.Dec)
+    
+        return x, y, z
+
+    def _convert_Cartesian_to_RADec(self, x, y, z):
+    
+        # Normalize back to unit sphere
+        R = np.sqrt(x**2 + y**2 + z**2)
+        
+        Dec = np.arcsin(z/R)
+        RA = np.arctan2(y/R,x/R)
+        # Check to make sure RA is the right data type & make sure we return the angle in the right quadrant
+        try:
+            RA[RA<0]+=2.*np.pi
+        except TypeError:
+            if RA<0: RA+=2.*np.pi
+            
+        return RA, Dec
+
+    def calculate_center_of_mass(self):
+        x, y, z = self._convert_RADec_to_Cartesian()
+
+        mean_x, mean_y, mean_z = x.mean(), y.mean(), z.mean()
+
+        self.center_of_mass = self._convert_Cartesian_to_RADec(mean_x, mean_y, mean_z)
+        return self.center_of_mass
+
+if __name__ == "__main__":
+    filename = "hardsurvey.txt"
+    data = pd.read_csv(filename, comment='#', names=['ID', 'RA', 'DEC'], delimiter=' ')
+    degtorad = np.pi/180.
+
+    our_catalog = Catalog(data['RA']*degtorad, data['DEC']*degtorad)
+    print(our_catalog.calculate_center_of_mass())
