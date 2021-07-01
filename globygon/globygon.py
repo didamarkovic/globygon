cf0a6b3d0f799f9dc5ea4ea38fefd5effb7807ac
Dida Markovic <markovic@jpl.nasa.gov>
2021-06-30 17:58:04 -0700

    Add function to calculate distances on sky. Update Demo to demo this. Test still todo.

--- globygon/globygon.py
+++ globygon/globygon.py
@@ -18,6 +18,8 @@ class Catalog:
         self.RA = RA
         self.Dec = Dec
 
+        self.x, self.y, self.z = None, None, None
+
         self.center_of_mass = None
 
     def _convert_RADec_to_Cartesian(self):
@@ -26,11 +28,11 @@ class Catalog:
         calculate the center-of-mass
         """
     
-        x = np.cos(self.Dec)*np.cos(self.RA)
-        y = np.cos(self.Dec)*np.sin(self.RA)
-        z = np.sin(self.Dec)
+        self.x = np.cos(self.Dec)*np.cos(self.RA)
+        self.y = np.cos(self.Dec)*np.sin(self.RA)
+        self.z = np.sin(self.Dec)
     
-        return x, y, z
+        return self.x, self.y, self.z
 
     def _convert_Cartesian_to_RADec(self, x, y, z):
         """
@@ -83,14 +85,55 @@ class Catalog:
         Returns: 
             list: RA/Dec of center-of-mass in radians
         """
-        x, y, z = self._convert_RADec_to_Cartesian()
+
+        # Convert RA/Dec of the catalogue entries to Cartesian and save if not yet done
+        if self.x is None or self.y is None or self.z is None:
+            self._convert_RADec_to_Cartesian()
 
         # Calculate Cartesian center of mass
-        mean_x, mean_y, mean_z = x.mean(), y.mean(), z.mean()
+        mean_x, mean_y, mean_z = self.x.mean(), self.y.mean(), self.z.mean()
+        self._com_Cart = mean_x, mean_y, mean_z
 
         # Convert to RA&Dec and project to sphere (simply by setting r=1)
         self.center_of_mass = self._convert_Cartesian_to_RADec(mean_x, mean_y, mean_z)
         return self.center_of_mass
+    
+    def calculate_dist_from_com(self,point=None):
+        """ 
+        With a Catalog class instantiated, a list of RA/Dec values
+        initialized and a calculated centre of mass, this method returns the 
+        distance of the center-of-mass to the RA/Dec points
+
+        Args:
+            point (tuple): arbitrary point (if different from the com) to calculate distance from
+                           RA/Dec in radians expected
+
+        Returns: 
+            list: distances of the RA/Dec points to the center-of-mass in radians
+        
+        """
+
+        # First catalogue to Cartesian if not already done
+        if self.x is None or self.y is None or self.z is None:
+            self._convert_RADec_to_Cartesian()
+
+        if point is None:
+            # Get the center of mass if the point is not given
+            self.calculate_center_of_mass()
+            point = self._com_Cart
+        else:
+            # Convert to Cartesian from radec
+            point = [np.cos(point[1])*np.cos(point[0]),
+                     np.cos(point[1])*np.sin(point[0]),
+                     np.sin(point[1])]
+ 
+        # Calculate Cartesian distances
+        dx, dy, dz = self.x-point[0], self.y-point[1], self.z-point[2]
+        dists = np.sqrt(dx**2 + dy**2 + dz**2)
+
+        # Project to R=1 sphere
+        R = 1
+        return 2.*R*np.arcsin((0.5*dists/R))
 
 if __name__ == "__main__":
     import argparse
